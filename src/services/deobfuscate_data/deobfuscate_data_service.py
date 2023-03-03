67b6d6fd13f77d1e0481f4f8c6dc1a75bf615951
Ig0or <igorprovensi@gmail.com>
2023-03-02 21:27:01 -0300

    feat: service to deobfuscate message

--- src/services/deobfuscate_data/deobfuscate_data_service.py
+++ src/services/deobfuscate_data/deobfuscate_data_service.py
@@ -1,7 +1,3 @@
-# Standard
-from typing import Callable
-import json
-
 # Third Party
 from cryptography.fernet import Fernet
 from decouple import config
@@ -12,22 +8,14 @@ class DeobfuscateDataService:
     __fernet_instance = Fernet(key=__fernet_key)
 
     @classmethod
-    def __encode_value(cls, value: str) -> bytes:
-        encoded_value = value.encode()
-
-        return encoded_value
-
-    @classmethod
     def __decode_value(cls, value: bytes) -> str:
         decoded_value = value.decode()
 
         return decoded_value
 
     @classmethod
-    def deobfuscate_value(cls, value: str) -> str:
-        encoded_value = cls.__encode_value(value=value)
-
-        deobfuscated_value = cls.__fernet_instance.decrypt(token=encoded_value)
+    def deobfuscate_value(cls, value: bytes) -> str:
+        deobfuscated_value = cls.__fernet_instance.decrypt(token=value)
         decoded_value = cls.__decode_value(value=deobfuscated_value)
 
         return decoded_value
