d0cc0bcb8605a116a4f6640dc2d9d952fb72b922
Ig0or <igorprovensi@gmail.com>
2023-03-02 21:28:14 -0300

    feat: kafka infra

--- src/infrastructure/kafka/kafka_infrastructure.py
+++ src/infrastructure/kafka/kafka_infrastructure.py
@@ -3,22 +3,24 @@ from decouple import config
 from kafka import KafkaConsumer
 
 # Local
-from src.services.obfuscate_data.obfuscate_data_service import DeobfuscateDataService
+from src.services.deobfuscate_data.deobfuscate_data_service import (
+    DeobfuscateDataService,
+)
 
-#
-# class KafkaInfrastructure:
-#     __producer = None
-#
-#     @classmethod
-#     def get_producer(cls) -> KafkaConsumer:
-#         if cls.__producer is None:
-#             producer_config = {
-#                 "bootstrap_servers": config("KAFKA_URL"),
-#                 "client_id": config("KAFKA_CLIENT_ID"),
-#                 "key_serializer": ObfuscateDataService.obfuscate_value,
-#                 "value_serializer": ObfuscateDataService.obfuscate_value,
-#             }
-#
-#             cls.__producer = KafkaConsumer(**producer_config)
-#
-#         return cls.__producer
+
+class KafkaInfrastructure:
+    __consumer = None
+
+    @classmethod
+    def get_consumer(cls) -> KafkaConsumer:
+        if cls.__consumer is None:
+            consumer_config = {
+                "bootstrap_servers": config("KAFKA_URL"),
+                "client_id": config("KAFKA_CLIENT_ID"),
+                "auto_offset_reset": "earliest",
+                "value_deserializer": DeobfuscateDataService.deobfuscate_value,
+            }
+
+            cls.__consumer = KafkaConsumer(**consumer_config)
+
+        return cls.__consumer
