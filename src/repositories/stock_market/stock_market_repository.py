25d6c9aded066ea521d77ee2c60ae2f19fe3558b
Ig0or <igorprovensi@gmail.com>
2023-03-06 14:15:48 -0300

    feat: query to update order on database

--- src/repositories/stock_market/stock_market_repository.py
+++ src/repositories/stock_market/stock_market_repository.py
@@ -1,9 +1,12 @@
 # Third Party
 from decouple import config
 from kafka import KafkaConsumer
+from typing import NoReturn
 
 # Local
+from src.domain.models.stock_market.order_model import OrderModel
 from src.infrastructure.kafka.kafka_infrastructure import KafkaInfrastructure
+from src.infrastructure.mongodb.mongodb_infrastructure import MongoDBInfrastructure
 
 
 class StockMarketRepository:
@@ -15,3 +18,15 @@ class StockMarketRepository:
         consumer.subscribe(topics=topic_name)
 
         return consumer
+
+    @staticmethod
+    async def update_order_on_database(order_model: OrderModel) -> NoReturn:
+        connection = MongoDBInfrastructure.get_connection()
+
+        query_filter = {"order_id": order_model["order_id"]}
+        updated_object = {"$set": order_model}
+
+        connection.update_one(filter=query_filter, update=updated_object)
+
+        return
+
