69f82bd7601458cb34fb0a7c590c4df4c8c230f4
Ig0or <igorprovensi@gmail.com>
2023-03-13 15:36:58 -0300

    feat: created price model and new attribute on order model

--- src/domain/models/alphavantage/price_model.py
+++ src/domain/models/alphavantage/price_model.py
@@ -1,5 +1,13 @@
+# Standard
 from typing import TypedDict
 
 
 class SymbolPriceModel(TypedDict):
-    pass
+    open: float
+    high: float
+    low: float
+    close: float
+    adjusted_close: float
+    volume: int
+    dividend_amount: float
+    split_coefficient: float
