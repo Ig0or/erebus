ac96badd27a4e5cd9f88f8c0f3ab9452201b8616
Ig0or <igorprovensi@gmail.com>
2023-03-17 18:51:07 -0300

    wip: exceptions treatment

--- src/infrastructure/kafka/kafka_infrastructure.py
+++ src/infrastructure/kafka/kafka_infrastructure.py
@@ -1,8 +1,13 @@
 # Third Party
 from decouple import config
 from kafka import KafkaConsumer
+from kafka.errors import NoBrokersAvailable
 
 # Local
+from src.domain.exceptions.infrastructure.infrastructure_exceptions import (
+    InfrastructureKafkaNoBrokersException,
+    InfrastructureUnexpectedException,
+)
 from src.services.deobfuscate_data.deobfuscate_data_service import (
     DeobfuscateDataService,
 )
@@ -13,16 +18,27 @@ class KafkaInfrastructure:
 
     @classmethod
     def get_consumer(cls) -> KafkaConsumer:
-        if cls.__consumer is None:
-            consumer_config = {
-                "bootstrap_servers": config("KAFKA_URL"),
-                "client_id": config("KAFKA_CLIENT_ID"),
-                "group_id": config("KAFKA_GROUP_ID"),
-                "auto_offset_reset": "earliest",
-                "key_deserializer": DeobfuscateDataService.deobfuscate_value,
-                "value_deserializer": DeobfuscateDataService.deobfuscate_value,
-            }
+        try:
+            if cls.__consumer is None:
+                consumer_config = {
+                    "bootstrap_servers": config("KAFKA_URL"),
+                    "client_id": config("KAFKA_CLIENT_ID"),
+                    "group_id": config("KAFKA_GROUP_ID"),
+                    "auto_offset_reset": "earliest",
+                    "key_deserializer": DeobfuscateDataService.deobfuscate_value,
+                    "value_deserializer": DeobfuscateDataService.deobfuscate_value,
+                }
 
-            cls.__consumer = KafkaConsumer(**consumer_config)
+                cls.__consumer = KafkaConsumer(**consumer_config)
 
-        return cls.__consumer
+            return cls.__consumer
+
+        except NoBrokersAvailable as exception:
+            raise InfrastructureKafkaNoBrokersException(
+                operation="KafkaInfrastructure::get_consumer", exception=exception
+            )
+
+        except Exception as exception:
+            raise InfrastructureUnexpectedException(
+                operation="KafkaInfrastructure::get_consumer", exception=exception
+            )
--- src/services/deobfuscate_data/deobfuscate_data_service.py
+++ src/services/deobfuscate_data/deobfuscate_data_service.py
@@ -2,8 +2,14 @@
 import json
 
 # Third Party
-from cryptography.fernet import Fernet
+from cryptography.fernet import Fernet, InvalidToken
 from decouple import config
+
+# Local
+from src.domain.exceptions.service.service_exceptions import (
+    ServiceInvalidFernetKeyException,
+    ServiceUnexpectedException,
+)
 
 
 class DeobfuscateDataService:
@@ -18,10 +24,23 @@ class DeobfuscateDataService:
 
     @classmethod
     def deobfuscate_value(cls, value: bytes) -> str:
-        deobfuscated_value = cls.__fernet_instance.decrypt(token=value)
-        decoded_value = cls.__decode_value(value=deobfuscated_value)
+        try:
+            deobfuscated_value = cls.__fernet_instance.decrypt(token=value)
+            decoded_value = cls.__decode_value(value=deobfuscated_value)
 
-        if "order_id" in decoded_value:
-            decoded_value = json.loads(decoded_value)
+            if "order_id" in decoded_value:
+                decoded_value = json.loads(decoded_value)
 
-        return decoded_value
+            return decoded_value
+
+        except InvalidToken as exception:
+            raise ServiceInvalidFernetKeyException(
+                operation="DeobfuscateDataService::deobfuscate_value",
+                exception=exception,
+            )
+
+        except Exception as exception:
+            raise ServiceUnexpectedException(
+                operation="DeobfuscateDataService::deobfuscate_value",
+                exception=exception,
+            )
