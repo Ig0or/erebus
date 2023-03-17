385436b8ad55ee115e0462d459bda2e28110c4d2
Ig0or <igorprovensi@gmail.com>
2023-03-17 16:22:46 -0300

    feat: changes on main file

--- erebus.py
+++ erebus.py
@@ -1,5 +1,6 @@
 # Standard
-from asyncio import new_event_loop, set_event_loop
+from asyncio import AbstractEventLoop, new_event_loop, set_event_loop
+from typing import NoReturn
 
 # Third Party
 from decouple import config
@@ -8,17 +9,29 @@ from pyfiglet import print_figlet
 # Local
 from src.services.stock_market.stock_market_service import StockMarketService
 
-if __name__ == "__main__":
-    event_loop = new_event_loop()
-    set_event_loop(loop=event_loop)
 
+def get_event_loop() -> AbstractEventLoop:
+    loop = new_event_loop()
+    set_event_loop(loop=loop)
+
+    return loop
+
+
+def print_consumer_name() -> NoReturn:
     print_figlet(text="Erebus", colors="30;37;235")
 
     kafka_url = config("KAFKA_URL")
     kafka_topic = config("KAFKA_TOPIC_NAME")
-    text_color = "\033[0;34m"
-    print(
-        f"{text_color}Starting consuming from URL: {kafka_url} - TOPIC: {kafka_topic}"
-    )
 
+    print("**********************************************************************")
+    print(f"Starting consuming from URL: {kafka_url} - TOPIC: {kafka_topic}")
+    print("**********************************************************************")
+
+    return
+
+
+if __name__ == "__main__":
+    print_consumer_name()
+
+    event_loop = get_event_loop()
     event_loop.run_until_complete(future=StockMarketService.start_consume())
