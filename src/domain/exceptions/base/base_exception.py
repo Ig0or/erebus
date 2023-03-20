e8a04407a36e7fcb21f220c8424f0951ca95e092
Ig0or <igorprovensi@gmail.com>
2023-03-19 22:59:18 -0300

    fix: function to read the property

--- src/domain/exceptions/base/base_exception.py
+++ src/domain/exceptions/base/base_exception.py
@@ -3,3 +3,15 @@ class BaseErebusException(Exception):
         self.__operation = operation
         self.__exception = exception
         self.__message = message
+
+    @property
+    def operation(self) -> str:
+        return self.__operation
+
+    @property
+    def exception(self) -> Exception:
+        return self.__exception
+
+    @property
+    def message(self) -> str:
+        return self.__message
