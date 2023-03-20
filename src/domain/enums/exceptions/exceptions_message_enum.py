8e8f8b479646a1da03bc3e9082b459ede80fc1f4
Ig0or <igorprovensi@gmail.com>
2023-03-20 13:45:56 -0300

    feat: new eror message

--- src/domain/enums/exceptions/exceptions_message_enum.py
+++ src/domain/enums/exceptions/exceptions_message_enum.py
@@ -15,7 +15,12 @@ class ServiceExceptionsMessageEnum(str, Enum):
     INVALID_FERNET_KEY = "Error decrypting topic value because the value signature isn't the same as your FERNET KEY in .env file"
 
 
+class TransportExceptionsMessageEnum(str, Enum):
+    RESPONSE_EXCEPTION = "There's a problem with the third party API called."
+
+
 class ErebusExceptionMessageEnum:
     general = GeneralExceptionMessageEnum
     infrastructure = InfrastructureExceptionsMessageEnum
     service = ServiceExceptionsMessageEnum
+    transport = TransportExceptionsMessageEnum
